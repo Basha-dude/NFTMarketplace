eae3b6f024f30c4354c227c3157710e50da3806b
Basha-dude <rebelbasha99@gmail.com>
2023-04-27 12:12:01 +0530

    upto updatingPrice function completed

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,10 +1,59 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+  //import "hardhat/console.sol";
 
-contract NFTMarketplace  {
+contract NFTMarketplace is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+    
+     uint256 listingPrice = 0.025 ether;
+     address payable owner;
+
+     mapping (uint256 => MarketItem ) private  idToMarketItem;
+
+     struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+     }
+
+     event MarketItemCreated (
+        uint256 indexed tokenId,
+        address  seller,
+        address  owner,
+        uint256 price,
+        bool sold
+     );
+
+     modifier onlyOwner {
+        require(msg.sender == owner,
+        "only owner can change"
+        );
+        _;
+     }
+
+
+
+    constructor () ERC721("Metaverse Tokens", "METT") {
+        owner =payable(msg.sender);
+    }
+
+    function updatingPrice(uint256 _listingPrice) public payable onlyOwner {
+        require(msg.sender == owner);
+        listingPrice = _listingPrice;
+    }
+
+
+
    
     
 }
