04a4fb22f1ec56fca6367966cb2dfdb87a784971
Basha-dude <rebelbasha99@gmail.com>
2023-05-03 18:34:25 +0530

    just completed get updating listing price

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // Uncomment this line to use console.log
-  //import "hardhat/console.sol";
+   //import "hardhat/console.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
 
@@ -44,14 +44,16 @@ contract NFTMarketplace is ERC721URIStorage {
 
 
     constructor () ERC721("Metaverse Tokens", "METT") {
-        owner =payable(msg.sender);
+        owner = payable(msg.sender);
     }
 
     function updatingListingPrice(uint256 _listingPrice) public  onlyOwner  {
       require(owner == msg.sender);
         listingPrice = _listingPrice;
     }
-  
+   function getListingPrice() public view returns (uint256 ) {
+         return listingPrice;
+   }
 
 
 
--- test/NFTMarketplacetest.js
+++ test/NFTMarketplacetest.js
@@ -34,7 +34,7 @@ describe("NFTMarketplace", function () {
     })
     
     describe("Updating listing price", () => {
-      let transaction, result
+      let transaction, result,listingPrice
       describe("Success",() => {
         beforeEach(async () => {
           listingPrice = ethers.utils.parseEther("0.0002")
@@ -44,15 +44,31 @@ describe("NFTMarketplace", function () {
         })
   
         it("should update the listing price",async () => {
-          expect(await nftMarketplace.listingPrice()).to.equal(ethers.utils.parseEther("0.0002").toString())
+          expect(await nftMarketplace.listingPrice()).to.equal(ethers.utils.parseEther("0.0002"))
+          console.log("Initial Listing Price: ", listingPrice.toString());
+          console.log("Expected Listing Price: ", ethers.utils.parseEther("0.0002"));
         })
       })
 
-      describe("Failure", async () => {
+       describe("Failure", async () => {
         it("should revert if called by a non-owner", async () => {
           await expect(nftMarketplace.connect(addr1).updatingListingPrice(listingPrice)).to.be.revertedWith("only owner can change");
         });
       })
+       describe('Get listing price function', () => { 
+        let transaction
+        beforeEach(async ()=> {
+          transaction = await nftMarketplace.getListingPrice()
+          
+        }) 
+        it("should pass the get listing price",async () => {
+          expect( await nftMarketplace.getListingPrice()).to.equal(await nftMarketplace.listingPrice())
+          console.log("GETTING PRICE :",transaction)
+          console.log("RETURN LISTING  PRICE :",await nftMarketplace.listingPrice())
+
+        })
+
+       })
     })
   })
 
